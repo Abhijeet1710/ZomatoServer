a73bb0fb0e9b81390ed32444da4165a57e7aa0a8
Abhijeet1710 <abhijeetkhamkar30@gmail.com>
2021-10-02 16:58:06 +0530

    google auth and restaurant api

--- API/Auth/index.js
+++ API/Auth/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import passport from "passport";
 
 const Router = express.Router();
 
@@ -47,7 +48,7 @@ Router.post("/signin", async(req,res) => {
 
     const user = await UserModel.findByEmailAndPassword(req.body.credentials);
 
-   //JWT Auth Token
+   //JWT Auth Tokesn
    const token = user.generateJwtToken();
 
    return res.status(200).json({token, status: "Success"});
@@ -56,5 +57,37 @@ Router.post("/signin", async(req,res) => {
     return res.status(500).json({error: error.message});
   }
 });
+
+
+/*
+Route         /google
+Descrip       Google Signin
+Params        None
+Access        Public
+Method        GET
+*/
+
+Router.get("/google", passport.authenticate("google",{
+scope: [
+  "https://www.googleapis.com/auth/userinfo.profile",
+  "https://www.googleapis.com/auth/userinfo.email"
+],
+})
+);
+
+/*
+Route         /google/callback
+Descrip       Google Signin callback
+Params        None
+Access        Public
+Method        GET
+*/
+
+Router.get("/google/callback", passport.authenticate("google",{failureRedirect: "/"}),
+(req,res) => {
+  return res.json({token: req.session.passport.user.token});
+}
+);
+
 
 export default Router;
